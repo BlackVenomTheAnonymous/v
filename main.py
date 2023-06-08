ac99e81675fa9b059f10fe42bd5bde2b9e153775
BlackVenomTheAnonymous <likhons304@gmail.com>
2023-06-09 00:25:29 +0600

    Update main.py

--- main.py
+++ main.py
@@ -17,30 +17,41 @@ def check_command(update, context):
     # Start the timer
     start_time = time.time()
 
-    # Make the API request
-    response = requests.get(api_url)
+    try:
+        # Make the API request
+        response = requests.get(api_url)
 
-    # Stop the timer and calculate the time taken
-    end_time = time.time()
-    time_taken = round(end_time - start_time, 2)
+        # Stop the timer and calculate the time taken
+        end_time = time.time()
+        time_taken = round(end_time - start_time, 2)
 
-    # Process the API response
-    if response.status_code == 200:
-        # Extract the result from the JSON response
-        result = response.json().get('result', '')
-        # Prepare the reply message
-        reply_message = f"Status: {'❌ DEAD' if result == 'DEAD' else '✅ ALIVE'} => {card_number}"
+        # Process the API response
+        if response.status_code == 200:
+            # Extract the result from the JSON response
+            result = response.json().get('result', '')
+            # Prepare the reply message
+            reply_message = f"Status: {'❌ DEAD' if result == 'DEAD' else '✅ ALIVE'} => {card_number}"
 
-                # Create the special button with the time taken
-        keyboard = [[telegram.InlineKeyboardButton("⚙️ Time Taken", callback_data=str(time_taken))]]
-        reply_markup = telegram.InlineKeyboardMarkup(keyboard)
+            # Create the special button with the time taken
+            keyboard = [[telegram.InlineKeyboardButton("⚙️ Time Taken", callback_data=str(time_taken))]]
+            reply_markup = telegram.InlineKeyboardMarkup(keyboard)
 
-        # Send the reply message and the button back to the user
-        context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message, reply_markup=reply_markup)
-    else:
-        reply_message = "An error occurred while processing the request."
-        # Send the error message back to the user
+            # Send the reply message and the button back to the user
+            context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message, reply_markup=reply_markup)
+        else:
+            reply_message = "An error occurred while processing the request."
+            # Send the error message back to the user
+            context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+
+    except requests.RequestException as e:
+        # Handle request exceptions, such as network errors
+        reply_message = f"An error occurred while making the API request: {str(e)}"
         context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
+
+    except Exception as e:
+        # Handle other exceptions
+        reply_message = f"An error occurred: {str(e)}"
+        context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)        context.bot.send_message(chat_id=update.effective_chat.id, text=reply_message)
 
 # Define the function to handle the callback query for the special button
 def button_callback(update, context):
